20110b1a4ef62a21cd080204e43b75edcea6bc64
yke5128 <0bv6t71k3854z1r@au.com>
2025-10-28 18:00:52 +0000

    523names

--- database.js
+++ database.js
@@ -193,8 +193,10 @@ fetch('names.json')
         }
       }
     });
+
+  // 都、練、虜がカウントできないので+3
     const output = document.getElementById('output');
-    output.textContent = `人名に使える漢字のうち、${globalKanjiSet.size}字／2999字が使われています`;
+    output.textContent = `人名に使える漢字のうち、${globalKanjiSet.size + 3}字／2999字が使われています`;
   })
   .catch(err => {
     document.getElementById('output').textContent = '読み込みエラー: ' + err.message;
